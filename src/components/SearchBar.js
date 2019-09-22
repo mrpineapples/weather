0b76cb22a9e841dab2dff05968b228e5b6c636bc
Michael Miranda <42720791+mrpineapples@users.noreply.github.com>
2019-09-21 22:02:52 -0400

    Minor cleanup

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,12 +4,9 @@ import "./SearchBar.css"
 class SearchBar extends React.Component {
   render() {
     return (
-      <form id="textbox"
-        onSubmit={this.props.submission}
-        className="searchbar">
-        <input className="input" type="text" placeholder="Enter City" onChange={this.props.changeCity}/>
-        <input className="submit" type="submit" value={this.props.buttonValue}
-          />
+      <form id="textbox" className="searchbar" onSubmit={this.props.submission} >
+        <input className="input" type="text" placeholder="Enter City" onChange={this.props.changeCity} />
+        <input className="submit" type="submit" value={this.props.buttonValue} />
       </form>
     )
   }
