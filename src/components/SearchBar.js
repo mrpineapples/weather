517269ae5b50c242aaac477e3629df3312d144c2
Michael Miranda <42720791+mrpineapples@users.noreply.github.com>
2019-09-22 00:03:10 -0400

    change class name

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,7 +4,7 @@ import "./SearchBar.css"
 class SearchBar extends React.Component {
   render() {
     return (
-      <form id="textbox" className="searchbar" onSubmit={this.props.submission} >
+      <form id="textbox" className="city-search" onSubmit={this.props.submission} >
         <input className="input" type="text" placeholder="Enter City" onChange={this.props.changeCity} />
         <input className="submit" type="submit" value={this.props.buttonValue} />
       </form>
